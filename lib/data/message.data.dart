bbf35608af6ccf4ae23c8c032122b05077d7aeb5
Andrew <145460945+Andrew-Ndong@users.noreply.github.com>
2025-11-24 17:40:46 +0100

    Ajout : Fichier contenant un tableau de message

---
+++ lib/data/message.data.dart
@@ -0,0 +1,92 @@
+final List<Map<String, dynamic>> messages = [
+  {
+    "fromMe": false,
+    "text": "Hey, t'es là ?",
+    "time": "10:12",
+  },
+  {
+    "fromMe": true,
+    "text": "Oui, je suis là. Quoi de neuf ?",
+    "time": "10:13",
+  },
+  {
+    "fromMe": false,
+    "text": "J'essaye de comprendre ton projet Flutter 😂",
+    "time": "10:15",
+  },
+  {
+    "fromMe": true,
+    "text": "Courage, Flutter ça s'apprivoise 💪",
+    "time": "10:16",
+  },
+  {
+    "fromMe": false,
+    "text": "Justement, je bloque sur la navigation entre pages.",
+    "time": "10:17",
+  },
+  {
+    "fromMe": true,
+    "text": "Tu veux passer des arguments ou juste changer d'écran ?",
+    "time": "10:17",
+  },
+  {
+    "fromMe": false,
+    "text": "Passer des arguments. Genre ouvrir une conversation.",
+    "time": "10:18",
+  },
+  {
+    "fromMe": true,
+    "text": "Ok, alors utilise Navigator.push + MaterialPageRoute.",
+    "time": "10:18",
+  },
+  {
+    "fromMe": false,
+    "text": "Ah oui, c’est simple en fait.",
+    "time": "10:19",
+  },
+  {
+    "fromMe": true,
+    "text": "Toujours 😆",
+    "time": "10:20",
+  },
+  {
+    "fromMe": false,
+    "text": "Tu peux me montrer un exemple vite fait ?",
+    "time": "10:21",
+  },
+  {
+    "fromMe": true,
+    "text": "Oui, regarde dans DiscussionsPage → onTap()",
+    "time": "10:22",
+  },
+  {
+    "fromMe": false,
+    "text": "Je viens de tester. Ça marche nickel !",
+    "time": "10:23",
+  },
+  {
+    "fromMe": true,
+    "text": "Parfait 😉",
+    "time": "10:24",
+  },
+  {
+    "fromMe": false,
+    "text": "Maintenant je veux gérer l’envoi de messages.",
+    "time": "10:25",
+  },
+  {
+    "fromMe": true,
+    "text": "Je te ferai un modèle Message + un contrôleur.",
+    "time": "10:26",
+  },
+  {
+    "fromMe": false,
+    "text": "Merci ! Tu gères.",
+    "time": "10:27",
+  },
+  {
+    "fromMe": true,
+    "text": "Normal, on avance 😄",
+    "time": "10:27",
+  },
+];
