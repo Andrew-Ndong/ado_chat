8ccde75fbd94dd795a60901e89f8a9efa77242d3
Andrew <145460945+Andrew-Ndong@users.noreply.github.com>
2025-11-24 12:44:57 +0100

    Ajout du fichier de configuration de couleur

---
+++ lib/themes/ligth_mode.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+ThemeData ligthMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    primary: Color.fromARGB(255,3, 61, 212),
+    secondary: Color.fromARGB(255, 24, 239, 255),
+    
+  )
+);
