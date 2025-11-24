0ed4b487184623c4bb3aab88d8515eeb9de991fe
Andrew <145460945+Andrew-Ndong@users.noreply.github.com>
2025-11-24 17:40:14 +0100

    Ajout : Fichier contenant un tableau de discussion

---
+++ lib/data/discussion.data.dart
@@ -0,0 +1,214 @@
+import 'package:flutter/material.dart';
+
+final List<Map<String, dynamic>> conversations = [
+  {
+    "icon": Icons.person,
+    "name": "Alexandra",
+    "lastMessage": "Je suis arrivée.",
+    "date": "Aujourd'hui",
+    "unread": 2
+  },
+  {
+    "icon": Icons.group,
+    "name": "Groupe Projet ESGIS",
+    "lastMessage": "Le code a été push.",
+    "date": "Aujourd'hui",
+    "unread": 5
+  },
+  {
+    "icon": Icons.person,
+    "name": "Brice",
+    "lastMessage": "Oui c’est bon.",
+    "date": "Hier",
+    "unread": 0
+  },
+  {
+    "icon": Icons.person,
+    "name": "Sarah",
+    "lastMessage": "On se voit à quelle heure ?",
+    "date": "Hier",
+    "unread": 1
+  },
+  {
+    "icon": Icons.person,
+    "name": "Mickael",
+    "lastMessage": "Parfait, merci !",
+    "date": "21/11",
+    "unread": 0
+  },
+  {
+    "icon": Icons.group,
+    "name": "Famille",
+    "lastMessage": "N'oubliez pas dimanche.",
+    "date": "Aujourd'hui",
+    "unread": 3
+  },
+  {
+    "icon": Icons.person,
+    "name": "Christopher",
+    "lastMessage": "Je t’appelle après.",
+    "date": "Hier",
+    "unread": 0
+  },
+  {
+    "icon": Icons.person,
+    "name": "Nathalie",
+    "lastMessage": "Ça marche.",
+    "date": "19/11",
+    "unread": 0
+  },
+  {
+    "icon": Icons.group,
+    "name": "Dev Flutter",
+    "lastMessage": "J’ai trouvé la solution.",
+    "date": "Aujourd'hui",
+    "unread": 6
+  },
+  {
+    "icon": Icons.person,
+    "name": "Hugo",
+    "lastMessage": "T’es dispo ?",
+    "date": "Aujourd'hui",
+    "unread": 1
+  },
+  {
+    "icon": Icons.person,
+    "name": "David",
+    "lastMessage": "Regarde ce lien.",
+    "date": "Hier",
+    "unread": 0
+  },
+  {
+    "icon": Icons.group,
+    "name": "Équipe Support",
+    "lastMessage": "Ticket résolu.",
+    "date": "20/11",
+    "unread": 4
+  },
+  {
+    "icon": Icons.person,
+    "name": "Sébastien",
+    "lastMessage": "Ok, merci.",
+    "date": "Aujourd'hui",
+    "unread": 0
+  },
+  {
+    "icon": Icons.person,
+    "name": "Léa",
+    "lastMessage": "Bonne nuit.",
+    "date": "Hier",
+    "unread": 0
+  },
+  {
+    "icon": Icons.group,
+    "name": "Sport",
+    "lastMessage": "Demain 18h.",
+    "date": "22/11",
+    "unread": 2
+  },
+  {
+    "icon": Icons.person,
+    "name": "Julien",
+    "lastMessage": "Tu confirmes ?",
+    "date": "Aujourd'hui",
+    "unread": 1
+  },
+  {
+    "icon": Icons.person,
+    "name": "Karim",
+    "lastMessage": "J'arrive.",
+    "date": "Hier",
+    "unread": 0
+  },
+  {
+    "icon": Icons.group,
+    "name": "Promo 2025",
+    "lastMessage": "Cours annulé.",
+    "date": "Hier",
+    "unread": 8
+  },
+  {
+    "icon": Icons.person,
+    "name": "Ingrid",
+    "lastMessage": "Merci beaucoup.",
+    "date": "Aujourd'hui",
+    "unread": 0
+  },
+  {
+    "icon": Icons.person,
+    "name": "Patrick",
+    "lastMessage": "Toujours partant ?",
+    "date": "20/11",
+    "unread": 0
+  },
+  {
+    "icon": Icons.person,
+    "name": "Olivier",
+    "lastMessage": "Top.",
+    "date": "Aujourd'hui",
+    "unread": 1
+  },
+  {
+    "icon": Icons.group,
+    "name": "Ami(e)s",
+    "lastMessage": "On fait comment ?",
+    "date": "Hier",
+    "unread": 3
+  },
+  {
+    "icon": Icons.person,
+    "name": "Elodie",
+    "lastMessage": "J’ai eu la réponse.",
+    "date": "22/11",
+    "unread": 0
+  },
+  {
+    "icon": Icons.person,
+    "name": "Anthony",
+    "lastMessage": "Tu peux m'envoyer ça ?",
+    "date": "Aujourd'hui",
+    "unread": 1
+  },
+  {
+    "icon": Icons.group,
+    "name": "Cloud Team",
+    "lastMessage": "Pipeline validé.",
+    "date": "Hier",
+    "unread": 7
+  },
+  {
+    "icon": Icons.person,
+    "name": "Cindy",
+    "lastMessage": "À plus.",
+    "date": "19/11",
+    "unread": 0
+  },
+  {
+    "icon": Icons.person,
+    "name": "Yann",
+    "lastMessage": "Tu peux checker ?",
+    "date": "21/11",
+    "unread": 2
+  },
+  {
+    "icon": Icons.group,
+    "name": "Sécurité Réseau",
+    "lastMessage": "Incident clos.",
+    "date": "Aujourd'hui",
+    "unread": 4
+  },
+  {
+    "icon": Icons.person,
+    "name": "Myriam",
+    "lastMessage": "Bien reçu.",
+    "date": "Hier",
+    "unread": 0
+  },
+  {
+    "icon": Icons.person,
+    "name": "Thomas",
+    "lastMessage": "Je fais ça demain.",
+    "date": "22/11",
+    "unread": 1
+  },
+];
